4ce00abc528cdc9e9426e268fceb6410d03911db
yoshifuji <t.yoshifuji@me.com>
2021-04-04 18:09:30 +0900

    Initial Commit

---
+++ todoSwiftUI/todoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoSwiftUIApp.swift
+//  todoSwiftUI
+//
+//  Created by ToruYoshifuji on 2021/04/04.
+//
+
+import SwiftUI
+
+@main
+struct todoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
